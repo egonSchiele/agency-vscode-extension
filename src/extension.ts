8038eaf014d052f96ba78fb8e338cb756583a446
Aditya Bhargava <adit@adit.io>
2026-01-01 22:08:11 -0600

    disable formatter for now

--- src/extension.ts
+++ src/extension.ts
@@ -1,16 +1,17 @@
-import * as vscode from 'vscode';
-import { ADLFormattingProvider } from './formatter';
+import * as vscode from "vscode";
+import { ADLFormattingProvider } from "./formatter";
 
 export function activate(context: vscode.ExtensionContext) {
-  console.log('ADL extension is now active');
+  console.log("ADL extension is now active");
 
-  // Register the document formatting provider for ADL files
+  /*   // Register the document formatting provider for ADL files
   const formatterProvider = vscode.languages.registerDocumentFormattingEditProvider(
     { scheme: 'file', language: 'adl' },
     new ADLFormattingProvider()
   );
 
   context.subscriptions.push(formatterProvider);
+ */
 }
 
 export function deactivate() {}
