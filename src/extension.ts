c6ecb82e04f66cb590ad8fb46b57dd6fc5363851
Aditya Bhargava <adit@adit.io>
2026-01-08 12:00:21 -0600

    use the agency formatter to format files

--- src/extension.ts
+++ src/extension.ts
@@ -4,14 +4,13 @@ import { AgencyFormattingProvider } from "./formatter";
 export function activate(context: vscode.ExtensionContext) {
   console.log("Agency extension is now active");
 
-  /*   // Register the document formatting provider for Agency files
+  // Register the document formatting provider for Agency files
   const formatterProvider = vscode.languages.registerDocumentFormattingEditProvider(
     { scheme: 'file', language: 'agency' },
     new AgencyFormattingProvider()
   );
 
   context.subscriptions.push(formatterProvider);
- */
 }
 
 export function deactivate() {}
